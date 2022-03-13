c0e1f2dcb413c76ea64dc9ce8d2864312bea36c5
rtheodoro <rtheodoro@usp.br>
2022-03-13 02:15:01 -0300

    texto da home modificada e site no shinyapps

--- R/mod_comocolaborar.R
+++ R/mod_comocolaborar.R
@@ -15,7 +15,7 @@ mod_comocolaborar_ui <- function(id){
     p("Existem várias formas de colaborar com a melhora deste aplicativo. Elas são:"),
     p(strong("1) Com código:"),"Se você entende de programação, pode acessar o github do projeto e contribuir com os ajustes que puder."),
     p(strong("2) Com base de dados:"), "Se você possui alguma base de dados, você pode contribuir enviando-a para que seja disponibilizada a todos."),
-    p(strong("3) Com dinheiro:"), "Para que o servidor suporte mais aplicações, é necessário pagar por um plano melhor."),
+    p(strong("3) Com dinheiro:"), "Para que o servidor suporte mais aplicações e acessos, é necessário pagar por um plano melhor."),
     p(strong("4) Com divulgação!"), "Compartilhe este projeto em suas redes sociais e com pessoas que se interessam pelo tema!")
   )
 
--- R/mod_home.R
+++ R/mod_home.R
@@ -11,10 +11,20 @@ mod_home_ui <- function(id){
   ns <- NS(id)
   tagList(
     p("Bem-vinde ao ", strong("Viscoop")),
-    p("Este projeto tem o objetivo de disponibilizar informações
-      estatísticas sobre as diversas Cooperativas Brasileiras utilizando informações da Receita Federal, Banco Central do Brasil e
-      RAIS.")
-
+    p("Este projeto pretende disponibilizar informações sobre as
+      diversas", strong("cooperativas brasileiras"), "utilizando informações do",
+      tags$a(href="https://www.bcb.gov.br/", "Banco Central do Brasil - Bacen",  target="_blank"),
+      ", da", tags$a(href="http://pdet.mte.gov.br/microdados-rais-e-caged?_ga=2.76313365.1278749075.1612490536-1284000383.1612490536", "Relação Anual de Informações Sociais - RAIS",  target="_blank"),
+      ", da", tags$a(href="https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj", "Receita Federal do Brasil - RFB",  target="_blank"),
+      "e outros."),
+    p("Também será feito o cruzamento dos dados e algumas análises estatísticas."),
+    p("Mais informações sobre as bases e estatísticas estão disponíveis nas abas à esquerda."),
+    p("Para estimular o aprendizado e garantir a transparência das informações,
+      este projeto está disponível inteiramente no ", tags$a(href="https://github.com/rtheodoro/Viscoop", "GitHub",  target="_blank"),
+      ". Isso possibilita a conferência de nossos cálculos, adaptação para outros projetos e que os
+      interessados possam utilizar o código onde desejar (desde que citada a
+     fonte, conforme", tags$a(href="https://github.com/rtheodoro/Viscoop/blob/main/LICENSE.md", "licença CC BY 4.0",  target="_blank"),")"),
+    p("O projeto está bem no ínicio e toda contribuição será bem-vinda.")
   )
 }
 
